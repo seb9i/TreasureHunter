eaecd65be061d3ba02f3350f171a1981577b0781
sebastian <sebastianp136@nycstudents.net>
2025-01-03 10:21:15 -0500

    TH-1: Added treasure class for hunt treasure implementation

---
+++ src/Treasure.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+public class Treasure {
+    private HashMap<String, Integer> treasure;
+
+    public Treasure(int weight1, int weight2, int weight3){
+        treasure.put("Necklace", 1);
+        treasure.put("Bottle", 4);
+        treasure.put("Stone", 9);
+    }
+
+}
